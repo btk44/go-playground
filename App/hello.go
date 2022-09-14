4e73f619598238c9ef3daf8849e50a6e37e907e0
bk <btko@tuta.io>
2022-09-14 21:21:34 +0200

    array and slice test

--- App/hello.go
+++ App/hello.go
@@ -3,5 +3,11 @@ package main
 import "fmt"
 
 func main() {
-    fmt.Println("Hello, World!")
+	arr := [3]int{1, 2, 3}
+	slice := []int{3, 4, 4}
+	arr[1] = 5
+	slice = append(slice, 7)
+
+	fmt.Println(arr)
+	fmt.Println(slice)
 }
